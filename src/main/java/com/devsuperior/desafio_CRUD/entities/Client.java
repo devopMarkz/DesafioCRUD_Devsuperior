b3f52761b487a28d48c48b8a39f3f5cd1045cb46
devopMarkz <marcosdev2002@gmail.com>
2024-11-29 16:44:27 -0300

    Criando entidade com Hibernate e realizando seeding na base de dados H2

---
+++ src/main/java/com/devsuperior/desafio_CRUD/entities/Client.java
@@ -0,0 +1,104 @@
+package com.devsuperior.desafio_CRUD.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "cpf", unique = true, nullable = false)
+    private String cpf;
+
+    @Column(name = "income")
+    private Double income;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "children")
+    private Integer children;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Client client = (Client) object;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,19 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Silva', '12345678901', 4500.00, '1990-05-14', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria Oliveira', '23456789012', 3200.00, '1985-03-22', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Souza', '34567890123', 8000.00, '1980-11-10', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Costa', '45678901234', 2500.00, '1995-07-18', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Pedro Mendes', '56789012345', 12000.00, '1978-01-25', 4);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Juliana Alves', '67890123456', 4000.00, '1992-09-05', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Rocha', '78901234567', 3500.00, '1988-06-11', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carla Dias', '89012345678', 6000.00, '1993-12-08', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Renato Lima', '90123456789', 7000.00, '1984-02-15', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernanda Ribeiro', '01234567890', 5200.00, '1989-08-30', 2);
